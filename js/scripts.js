304f02c61bc555bac71e9a99b9ba026ee047b0ef
Jason Brown and philip putnam <philip@>
2017-01-10 15:42:20 -0800

    add (this) for button functionality, create div btnHolder class

--- js/scripts.js
+++ js/scripts.js
@@ -35,33 +35,36 @@ $(document).ready(function() {
     });
   });
 
-
-  $(".col-md-4#javascript button").click(function() {
-    $(".col-md-4#javascript p").toggle();
+  $(".btnHolder").click(function(){
+    $(this).children("p").toggle();
   });
 
-  $(".col-md-4#operators button").click(function() {
-    $(".col-md-4#operators p").toggle();
-  });
-
-  $(".col-md-4#variables button").click(function() {
-    $(".col-md-4#variables p").toggle();
-  });
-
-  $(".col-md-4#varNamConv button").click(function() {
-    $(".col-md-4#varNamConv p").toggle();
-  });
-
-  $(".col-md-4#functions button").click(function() {
-    $(".col-md-4#functions p").toggle();
-  });
-
-  $(".col-md-4#methods button").click(function() {
-    $(".col-md-4#methods p").toggle();
-  });
-
-  $(".col-md-4#arguments button").click(function() {
-    $(".col-md-4#arguments p").toggle();
-  });
+  // $(".col-md-4#javascript button").click(function() {
+  //   $(".col-md-4#javascript p").toggle();
+  // });
+  //
+  // $(".col-md-4#operators button").click(function() {
+  //   $(".col-md-4#operators p").toggle();
+  // });
+  //
+  // $(".col-md-4#variables button").click(function() {
+  //   $(".col-md-4#variables p").toggle();
+  // });
+  //
+  // $(".col-md-4#varNamConv button").click(function() {
+  //   $(".col-md-4#varNamConv p").toggle();
+  // });
+  //
+  // $(".col-md-4#functions button").click(function() {
+  //   $(".col-md-4#functions p").toggle();
+  // });
+  //
+  // $(".col-md-4#methods button").click(function() {
+  //   $(".col-md-4#methods p").toggle();
+  // });
+  //
+  // $(".col-md-4#arguments button").click(function() {
+  //   $(".col-md-4#arguments p").toggle();
+  // });
 
 });
