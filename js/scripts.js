10859b4016cc5fbb3928ee0e7b39100dc31ec425
Jason Brown and philip putnam <jason@>
2017-01-10 11:28:20 -0800

    add buttons, jquery toggle(), and html content

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
     $(".col-md-4#javascript p").toggle();
   });
 
+  $(".col-md-4#operators button").click(function() {
+    $(".col-md-4#operators p").toggle();
+  });
+
+  $(".col-md-4#variables button").click(function() {
+    $(".col-md-4#variables p").toggle();
+  });
+
+  $(".col-md-4#varNamConv button").click(function() {
+    $(".col-md-4#varNamConv p").toggle();
+  });
+
+  $(".col-md-4#functions button").click(function() {
+    $(".col-md-4#functions p").toggle();
+  });
+
+  $(".col-md-4#methods button").click(function() {
+    $(".col-md-4#methods p").toggle();
+  });
+
+  $(".col-md-4#arguments button").click(function() {
+    $(".col-md-4#arguments p").toggle();
+  });
+
+
 });
